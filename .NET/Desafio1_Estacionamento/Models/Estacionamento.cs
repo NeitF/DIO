362d2f511e0812f18e2505af2056dabccaa504d4
Nathan Fonseca <nathan.silva@becomex.com.br>
2023-11-29 18:16:36 -0300

    desafio 1 .net
    ;

---
+++ .NET/Desafio1_Estacionamento/Models/Estacionamento.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1_Estacionamento.Models
+{
+    internal class Estacionamento
+    {
+        public decimal PrecoInicial { get; set; }
+        public decimal PrecoPorHora { get; set; }
+        public List<string> Veiculos { get; set; }
+
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        {
+            PrecoInicial = precoInicial;
+            PrecoPorHora = precoPorHora;
+            Veiculos = new List<string>();
+        }
+
+        public void adicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo: ");
+            Veiculos.Add(Console.ReadLine());
+        }
+
+        public void removerVeiculo()
+        {
+            Console.WriteLine("Digite o veículo para remover: ");
+            string remVeiculo = Console.ReadLine();
+
+            if (Veiculos.Contains(remVeiculo))
+            {
+                Console.WriteLine("Quantidade de horas que permaneceu estacionado: ");
+                int horas = int.Parse(Console.ReadLine());
+                
+                decimal precoFinal = PrecoInicial + (PrecoPorHora * horas);
+
+                Console.WriteLine($"O veículo {remVeiculo} foi removido e o preço total foi de {precoFinal + PrecoInicial}");
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("O veículo não foi encontrado");
+            }
+
+        }
+
+        public void listarVeiculo()
+        {
+            Console.WriteLine("Lista de veículos estacionados");
+            if (Veiculos.Any())
+            {
+                Veiculos.ForEach(v => Console.WriteLine(v));
+            }
+            else
+            {
+                Console.WriteLine("Nenhum veículo foi encontrado");
+            }
+        }
+    }
+}
---
+++ .NET/Desafio1_Estacionamento/Principal.cs
@@ -0,0 +1,66 @@
+﻿using Desafio1_Estacionamento.Models;
+
+public class Principal
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o preço inicial: ");
+        decimal precoInicial = decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o preço por hora: ");
+        decimal precoPorHora = decimal.Parse(Console.ReadLine());
+
+        Estacionamento e1 = new Estacionamento(precoInicial, precoPorHora);
+        bool continuarExecucao = true;
+        int opcao;
+
+
+        while (continuarExecucao)
+        {
+            Console.Clear();
+            Console.WriteLine("Digite a sua opção:" +
+                "\n1 - Cadastrar veículo " +
+                "\n2 - Remover veículo" +
+                "\n3 - Listar veículos" +
+                "\n4 - Encerrar");
+
+            try
+            {
+                opcao = int.Parse(Console.ReadLine());
+            }
+            catch(Exception e)
+            {
+                continue;
+            }
+
+            switch (opcao)
+            {
+                case 1:
+                    e1.adicionarVeiculo();
+                    break;
+                case 2:
+                    e1.removerVeiculo();
+                    break;
+                case 3:
+                    try
+                    {
+                        e1.listarVeiculo();
+                    }
+                    catch(ArgumentOutOfRangeException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    break;
+                case 4:
+                    continuarExecucao = false;
+                    break;
+                default:
+                    Console.WriteLine("A opção inserida é inválida");
+                    break;
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para continuar...");
+            Console.ReadLine();
+        }
+    }
+}
